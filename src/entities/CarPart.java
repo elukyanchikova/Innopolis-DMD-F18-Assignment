5a4065b76a4d84dcf65ba97adaa60de14daf0b6d
Ruslan Shakirov <shakirovrrr@outlook.com>
2018-11-26 20:03:04 +0300

    WID is set to -1 on initialization

--- src/entities/CarPart.java
+++ src/entities/CarPart.java
@@ -2,66 +2,67 @@ package entities;
 
 public class CarPart {
 
-    private int PartID;
-    private String PartName;
-    private float PartPrice;
-    private String PartManufacturer;
-    private int ProviderID;
-    private int WID;
+	private int PartID;
+	private String PartName;
+	private float PartPrice;
+	private String PartManufacturer;
+	private int ProviderID;
+	private int WID;
 
-    public CarPart(int partID, String partName, float partPrice, String partManufacturer, int providerID) {
-        setPartID(partID);
-        setPartName(partName);
-        setPartPrice(partPrice);
-        setPartManufacturer(partManufacturer);
-        setProviderID(providerID);
-    }
+	public CarPart(int partID, String partName, float partPrice, String partManufacturer, int providerID) {
+		setPartID(partID);
+		setPartName(partName);
+		setPartPrice(partPrice);
+		setPartManufacturer(partManufacturer);
+		setProviderID(providerID);
+		WID = -1;
+	}
 
-    public int getPartID() {
-        return PartID;
-    }
+	public int getPartID() {
+		return PartID;
+	}
 
-    public void setPartID(int partID) {
-        PartID = partID;
-    }
+	public void setPartID(int partID) {
+		PartID = partID;
+	}
 
-    public String getPartName() {
-        return PartName;
-    }
+	public String getPartName() {
+		return PartName;
+	}
 
-    public void setPartName(String partName) {
-        PartName = partName;
-    }
+	public void setPartName(String partName) {
+		PartName = partName;
+	}
 
-    public float getPartPrice() {
-        return PartPrice;
-    }
+	public float getPartPrice() {
+		return PartPrice;
+	}
 
-    public void setPartPrice(float partPrice) {
-        PartPrice = partPrice;
-    }
+	public void setPartPrice(float partPrice) {
+		PartPrice = partPrice;
+	}
 
-    public String getPartManufacturer() {
-        return PartManufacturer;
-    }
+	public String getPartManufacturer() {
+		return PartManufacturer;
+	}
 
-    public void setPartManufacturer(String partManufacturer) {
-        PartManufacturer = partManufacturer;
-    }
+	public void setPartManufacturer(String partManufacturer) {
+		PartManufacturer = partManufacturer;
+	}
 
-    public int getProviderID() {
-        return ProviderID;
-    }
+	public int getProviderID() {
+		return ProviderID;
+	}
 
-    public void setProviderID(int providerID) {
-        ProviderID = providerID;
-    }
+	public void setProviderID(int providerID) {
+		ProviderID = providerID;
+	}
 
-    public int getWID() {
-        return WID;
-    }
+	public int getWID() {
+		return WID;
+	}
 
-    public void setWID(int WID) {
-        this.WID = WID;
-    }
+	public void setWID(int WID) {
+		this.WID = WID;
+	}
 }
