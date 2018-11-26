40628bd1509dd26fe55a568d5aebe9aafdf9955b
Ruslan Shakirov <shakirovrrr@outlook.com>
2018-11-26 22:47:17 +0300

    Application skeleton finished

--- src/graphicalUI/MainPage.java
+++ src/graphicalUI/MainPage.java
@@ -2,13 +2,23 @@ package graphicalUI;
 
 import database.DatabaseQueries;
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 
 public class MainPage extends Application {
 
@@ -18,6 +28,8 @@ public class MainPage extends Application {
 	Button generateDataBtn;
 	@FXML
 	FlowPane controls;
+	@FXML
+	TableView outputTable;
 
 	Button[] queryButtons;
 
@@ -29,14 +41,30 @@ public class MainPage extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		queries = new DatabaseQueries();
+		AnchorPane layout = null;
+		try {
+			layout = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+		} catch (IOException e) {
+			System.err.println("Cannot load layout.");
+			System.exit(-1);
+		}
 
+		if (layout == null) {
+			System.err.println("Failed to load layout.");
+			System.exit(-1);
+		}
+
+		Scene scene = new Scene(layout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 	@FXML
 	void generateData() {
+		queries = new DatabaseQueries();
 		queries.createSample();
 		queries.FillSample();
+		addQueryButtons();
 	}
 
 	void addQueryButtons() {
@@ -50,29 +78,63 @@ public class MainPage extends Application {
 	}
 
 	void showQuery(int n) {
-		ResultSet resultSet;
+		ResultSet resultSet = null;
 		switch (n) {
 			case 1:
+				resultSet = queries.Query01();
 				break;
 			case 2:
+//				resultSet = queries.Query02(getRequestedDateSeconds());
 				break;
 			case 3:
+//				resultSet=queries.Query03(getRequestedDateSeconds());
 				break;
 			case 4:
+				resultSet = queries.Query04(getRequestedDateSeconds());
 				break;
 			case 5:
+				resultSet = queries.Query05(getRequestedDateSeconds());
 				break;
 			case 6:
+//				resultSet=queries.Query06(getRequestedDateSeconds());
 				break;
 			case 7:
+//				resultSet=queries.Query07();
 				break;
 			case 8:
+//				resultSet=queries.Query08();
 				break;
 			case 9:
+				resultSet = queries.Query09();
 				break;
 			case 10:
+//				resultSet=queries.Query10();
 				break;
+		}
+
+		assert resultSet != null;
+
+	}
+
+	void updateTable(ResultSet resultSet) {
+		outputTable.getColumns().clear();
+
+		try {
+			for (int i = 0; i < resultSet.getMetaData().getColumnCount(); i++) {
+				final int j = i;
+				TableColumn<ObservableList, String> column =
+						new TableColumn(resultSet.getMetaData().getColumnName(j + 1));
+				column.setCellValueFactory(param ->
+						new SimpleStringProperty(param.getValue().get(j).toString()));
+
+				outputTable.getColumns().add(column);
+			}
+		} catch (SQLException | ClassCastException e) {
+			e.printStackTrace();
 		}
 	}
 
+	long getRequestedDateSeconds() {
+		return Timestamp.valueOf(datePicker.getValue().atStartOfDay()).getTime() / 1000;
+	}
 }
